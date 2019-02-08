cc02e3c4a878498edfa89188ea4a5e4856fca414
Florian Blasius <flovanpt@posteo.de>
2019-02-08 13:11:13 +0100

    [orbgl_shapes] add Shape trait to prelude.

--- orbgl_shapes/src/prelude.rs
+++ orbgl_shapes/src/prelude.rs
@@ -1,6 +1,6 @@
 
 pub use crate::{
     structs::{Border, BorderBuilder, Brush, Bordered, Rect, Position, Size, GradientStop, Thickness},
-    shapes::{ImageElement, Rectangle},
+    shapes::{Shape, ImageElement, Rectangle},
     shape_renderer::ShapeRenderer,
 };
