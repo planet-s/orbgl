ad190c3a27f2702771d8dede4c58ffc89e80f21f
Florian Blasius <florian.blasius@ergosign.de>
2019-05-28 13:59:09 +0200

    Up font.

--- orbgl_api/src/font.rs
+++ orbgl_api/src/font.rs
@@ -2,5 +2,15 @@
 #[derive(Copy, Clone, Default, Debug)]
 pub struct Font {}
 
+impl Font {
+    pub fn from_data(_: Box<[u8]>) -> Result<Font, String> {
+        Ok(Font {})
+    }
+
+    pub fn from_path<P: AsRef<Path>>(_: P) -> Result<Font, String> {
+         Ok(Font {})
+    }
+}
+
 #[cfg(not(target_arch = "wasm32"))]
 pub use orbfont::Font;
