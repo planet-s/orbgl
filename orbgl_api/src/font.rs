5f7ab2c6e071332fdbcb21956a25aaf057b00bd6
Florian Blasius <florian.blasius@ergosign.de>
2019-05-29 07:08:21 +0200

    Add font family.

--- orbgl_api/src/font.rs
+++ orbgl_api/src/font.rs
@@ -2,15 +2,22 @@ use std::path::Path;
 
 #[cfg(target_arch = "wasm32")]
 #[derive(Copy, Clone, Default, Debug)]
-pub struct Font {}
+pub struct Font {
+    pub family: String
+}
 
 impl Font {
+    pub fn from_family(family: impl Into<String>) -> Font {
+        Font {
+            family: family.into()
+        }  
+    }
     pub fn from_data(_: Box<[u8]>) -> Result<Font, String> {
-        Ok(Font {})
+        Ok(Font { family: String::default() })
     }
 
     pub fn from_path<P: AsRef<Path>>(_: P) -> Result<Font, String> {
-         Ok(Font {})
+         Ok(Font {  family: String::default() })
     }
 }
 
